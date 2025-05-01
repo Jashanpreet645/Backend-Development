d83f7a75a041f9f4c5469202e0b2874ba3de8464
jashanpreet645 <jashanpreetlehra@gmail.com>
2025-05-01 15:42:53 +0530

    new topics added

--- REST/index.js
+++ REST/index.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
@@ -128,4 +131,25 @@ app.get('/posts/:id', (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => p.id == id);
     res.render('show.ejs', { post , id});
+})
+
+app.get('/posts/:id/edit', (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => p.id == id);
+    res.render('edit.ejs', { post });
+})
+
+app.patch('/posts/:id', (req, res) => {
+    let {id} = req.params;
+    let newcontent = req.body.content;
+    let post = posts.find((p) => p.id == id);
+    post.content = newcontent;
+    console.log(post);
+    res.redirect('/posts');
+})
+
+app.delete('/posts/:id', (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id != p.id);
+    res.redirect('/posts');
 })
